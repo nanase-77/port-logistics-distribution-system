0ed1d1bbc8ce6c041f01bbf654d8b8e05b12ce88
fenhualuoye <m17301731194@163.com>
2026-05-05 08:46:52 +0800

    后端

--- src/main/java/com/smu/portlogisticsdistributionsystem/service/PortService.java
+++ src/main/java/com/smu/portlogisticsdistributionsystem/service/PortService.java
@@ -1,10 +1,11 @@
 package com.smu.portlogisticsdistributionsystem.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.smu.portlogisticsdistributionsystem.dto.PortDTO;
 import com.smu.portlogisticsdistributionsystem.dto.PortQueryDTO;
 import com.smu.portlogisticsdistributionsystem.entity.Port;
-public interface PortService {
+public interface PortService extends IService<Port> {
     Page<Port> select(int pageNum, int pageSize, PortQueryDTO portQueryDTO);
 
     void add(PortDTO portDTO);
